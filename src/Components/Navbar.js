128d9d3174f757a36c4af442bbca107b651ab236
skrewitup <hardikgossain10@gmail.com>
2023-06-26 21:31:26 -0400

    href correction

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg  navbar-${props.theme} bg-${props.theme}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {props.title}
         </a>
         <button
@@ -34,7 +34,7 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
